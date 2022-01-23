9a43625f69ffeb59d2ffae178b56cedb4a380c89
Max Zernyaev <LentyaiPrOO@yandex.ru>
2022-01-23 17:47:15 +0300

    testing user input and filter

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -2,9 +2,28 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 
 import App from './App'
+import userEvent from '@testing-library/user-event'
 
-test('renders learn react link', () => {
-	render(<App/>)
-	const linkElement = screen.getByText(/learn react/i)
-	expect(linkElement).toBeInTheDocument()
+describe('App component', () => {
+	it('renders learn react link', () => {
+		render(<App/>)
+		expect(screen.getByRole('list')).toBeInTheDocument()
+		expect(screen.getByText('Find course:')).toBeInTheDocument()
+	})
+
+	it('should typing in Searchbox works', function () {
+		render(<App />)
+		expect(screen.queryByDisplayValue(/React/)).toBeNull()
+		userEvent.type(screen.getByRole('textbox'), 'React')
+		expect(screen.getByDisplayValue(/React/)).toBeInTheDocument()
+	})
+
+	it('should search filter is working', function () {
+		render(<App />)
+		expect(screen.getByText(/html/)).toBeInTheDocument()
+		userEvent.type(screen.getByRole('textbox'), 'html')
+		expect(screen.queryByText(/js/)).toBeNull()
+		expect(screen.getByText(/html/)).toBeInTheDocument()
+	})
 })
+
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import List from '../List'
 import Search from '../Search'
@@ -6,15 +6,20 @@ import Search from '../Search'
 const data = ['html', 'css', 'js']
 
 function App() {
-	const [search, setSearch] = useState('')
+	const [search, setSearch] = useState<string>('')
+	const [items, setItems] = useState<string[]>(data)
+
+	useEffect(() => {
+		setItems(data.filter(el => el.toLowerCase().includes(search.toLowerCase())))
+	}, [search])
 
 	return (
 		<div className="App">
 			<div className="App-header">
 				<Search value={search} onChange={(e) => setSearch(e.target.value)}>
-					Find course
+					Find course:
 				</Search>
-				<List items={data} />
+				<List items={items} />
 			</div>
 		</div>
 	)
